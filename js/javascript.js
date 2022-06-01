84294134b9503e0f555408a6fe7aa5e7ae81004c
Svetlana Gaydey <lana84@ukr.net>
2022-06-01 17:01:53 +0300

    Plagin choices.js implemented to gallery dropdown

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,10 @@
-console.log("hello")
+console.log("hello");
+// Section Gallery - Dropdown choices
+const defaultSelect = () => {
+    const elementGallery = document.querySelector('#gallery__select');
+    const choiceGallery = new Choices(elementGallery, {
+      searchEnabled: false,
+      position: 'bottom'
+    });
+  }
+defaultSelect();
