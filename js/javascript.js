28e2b8173fdc448a6a9c2e8bdc3911ce853353e6
Svetlana Gaydey <lana84@ukr.net>
2022-06-05 16:59:01 +0300

    Slider in Gallery section completed

--- js/javascript.js
+++ js/javascript.js
@@ -8,3 +8,44 @@ const defaultSelect = () => {
     });
   }
 defaultSelect();
+
+// Section Hero slider
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+    spaceBetween: 0,
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
+
+// Section Gallery slider
+
+let gallerySlider = new Swiper(".gallery__slider", {
+  slidesPerView: 3,
+  spaceBetween: 52,
+  slidesPerGroup: 3,
+  spaceAfterLast: 0,
+  
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
